c94fdf7e8e677a2993d9d9cc818fce1217552586
frankfurillo <gunnar.franklin@gmail.com>
2015-04-01 22:50:35 +0200

    tetris stuff

--- gruntfile.js
+++ gruntfile.js
@@ -4,23 +4,38 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-    my_target: {
-      files: {
+		my_target: {
+		files: {
         'build/zz.min.js': ['lib/*.js', 'curve.js']
-      }
-    }
-  }
-  ,
+		}
+		}
+	}
+	,
 	jshint: {
 		all: [ 'lib/*.js']
+	},
+	sass: {
+			dist: {
+				files: {
+					'style/style.css' : 'sass/style.scss'
+				}
+			}
+	},
+	watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
 	}
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','watch']);
 
 };
--- tetris/js/tetris.js
+++ tetris/js/tetris.js
@@ -1,27 +1,32 @@
 ﻿requirejs.config({
-    baseUrl: '../../',
+    baseUrl: '../',
     paths: {
         lib: 'lib'
     }
 });
-define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], function ($, zz, zzUtil, zzInteraction,zzDebug) {
+define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug','lib/letterbox'], function ($, zz, zzUtil, zzInteraction,zzDebug,letterbox) {
+    function calcSize() {
+        //$('#canvas1').css('width', $(document).width() - 50);
+        //$('#canvas1').css('height', $(document).height() - 50);
+        return zz.world.w / 12;
+    }
+
     $("document").ready(function () {
         zz.init($("#canvas1")[0]);
-
         var
-            board = [14, 19],
+            board = [12, 18],
             boardMap = [[]];
-            block = 30,
-            miniCube = [0, 0, 1, 0, 1, 1, 0, 1],
-            longy = [[0, 0, 0, 1, 0, 2, 0, 3], [0, 0, 1,0, 2, 0, 3, 0]],
-            button = [0, 0, block * 3, 0, block * 3, block, 0, block],
-            tetriColors = ['#F5ccdd', '#e59acc', '#00ddff'];
+        block = calcSize(),
+        miniCube = [0, 0, 1, 0, 1, 1, 0, 1],
+        longy = [[0, 0, 0, 1, 0, 2, 0, 3], [0, 0, 1,0, 2, 0, 3, 0]],
+        zed = [[0, 1, 1, 1, 1, 2, 2, 2], [2, 0, 1, 1, 2, 1, 1, 2]],
+        zedReverse = [[0, 0, 0, 1, 1, 1, 1, 2], [1, 1, 2, 1, 0, 2, 1, 2]],
+        cube = [[0, 0, 1, 0, 0, 1, 1, 1]],
+        theL = [[0, 1, 0, 2, 1, 2, 2, 2], [1, 0, 2, 0, 1, 1, 1, 2], [0, 1, 1, 1, 2, 1, 2, 2], [1, 0, 1, 1, 1, 2, 0, 2]],
+        theLReverse = [[1, 0, 1, 1, 1, 2, 2, 2], [0, 1, 1, 1, 2, 1, 0, 2], [0, 0, 1, 0, 1, 1, 1, 2], [0, 2, 1, 2, 2, 2, 2, 1]],
+        button = [0, 0, block * 3, 0, block * 3, block, 0, block],
+        tetriColors = ['#F5ccdd', '#e59acc', '#00ddff'];
 
-        function blockify(shape) {
-            return  shape.map(function (a) {
-                return a * 40;
-            });
-        }
 
         Array.prototype.randItem = function () {
             return this[Math.floor(Math.random() * this.length)];
@@ -66,7 +71,7 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
             var bWidth= board[0];
             var bHeight = board[1];
             for (var i = 0; i < bWidth ; i++) {
-                boardMap.push([]);
+                boardMap[i]=[];
                 for (var a = 0; a < bHeight; a++) {
                     var boardBlock = new zz.stickFigure(
                         i * block,
@@ -75,7 +80,7 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
                         block,
                         '#bbbbbb',
                         '#555555',
-                        blockify(miniCube),
+                        zzUtil.blockify(miniCube),
                         0,
                         {
                             x: 0,
@@ -84,6 +89,7 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
                     );
                     boardMap[i].push([]);
                     boardMap[i][a]=boardBlock;
+
                     zz.world.items.push(boardBlock)
                 }
             }
@@ -92,7 +98,6 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
         drawBoard();
 
 
-        //zz.world.items.push(seedCube());
         var buttons = [
             seedSimpleShape({
                 x: zz.world.w - zzUtil.shapeSize(button).w,
@@ -100,50 +105,181 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
             }),
 
             seedSimpleShape({ x: 0, y: zz.world.h - zzUtil.shapeSize(button).h, shape: button, color: '#eeeeee', borderColor: '#666666' }),
-            seedSimpleShape({ x: zz.world.w/2, y: zz.world.h - zzUtil.shapeSize(button).h, shape: button, color: '#eeee11', borderColor: '#666666' })
-            ];
+            seedSimpleShape({ x: (zz.world.w/2) - 40 , y: zz.world.h - zzUtil.shapeSize(button).h, shape: button, color: '#eeee11', borderColor: '#666666' })
+        ];
+
+       
 
         var gameControl = {
-                defaultBlockColor : '#aaaaaa',
-                currentFallingItem:{
-                    variShape: null,
-                    rotation: 0, //index,
-                    offsetX:0,
-                    offsetY: 0,
-                    rotate: function () {
-                        if (this.rotation < this.variShape.length - 1) {
-                            this.rotation++;
-                        }
-                        else {
-                            this.rotation = 0;
-                        }
+            state:-1, //all above zero is running, -1 notstarted, -2 gameovcer
+            defaultBlockColor: '#aaaaaa',
+            currentFallingItem:{
+                variShape: null,
+                rotation: 0, //index,
+                offsetX:0,
+                offsetY: 0,
+                color: '#ff2299',
+                rotate: function () {
+                    if (this.rotation < (this.variShape.length - 1)) {
+                        this.rotation++;
+                    }
+                    else {
+                        this.rotation = 0;
+                    }
+                },
+                getShape: function () {
+                    return this.variShape[this.rotation];
+                }
+            }
+            ,
+            seedFallingItem: function () {
+                if (this.state <0 ) {
+                    return;
+                }
+                //clearPrevious first
+                //zzDebug.debug("rotation", this.currentFallingItem.rotation);
+                //zzDebug.debug("shapelength", this.currentFallingItem.variShape.length);
+                this.clearAllNonBlocked();
+                var currShape = this.currentFallingItem.getShape(); 
+                var x = this.currentFallingItem.offsetX;
+                var y = this.currentFallingItem.offsetY;
+
+                for (var i = 0; i < currShape.length; i += 2) {
+                    if ((currShape[i] + x) < boardMap.length && (currShape[i + 1] + y) < boardMap[0].length) {
+                        boardMap[currShape[i] + x][currShape[i + 1] + y].fillColor = this.currentFallingItem.color;
                     }
                 }
-            ,
-                seedFallingItem: function () {
-                    //clearPrevious first
-                    this.clearAllNonBlocked();
-                    var currShape = this.currentFallingItem.variShape[this.currentFallingItem.rotation]; //cant do this here if we shall repeat this function....
-                    var x = this.currentFallingItem.offsetX;
-                    var y = this.currentFallingItem.offsetY;
 
-                    for (var i = 0; i < currShape.length; i += 2) {
-                        boardMap[currShape[i] + x][currShape[i + 1] + y].fillColor = "#ff2299";
-                    }
-
-                }
+            }
             ,
-                clearAllNonBlocked: function () {
-                    for(var i=0;i < boardMap.length ;i++){
-                        for (var a = 0 ; a< boardMap[i].length ; a++){
+            clearAllNonBlocked: function () {
+                for(var i=0;i < boardMap.length ;i++){
+                    for (var a = 0 ; a < boardMap[i].length ; a++) {
+                        if (!boardMap[i][a].blocked) {
                             boardMap[i][a].fillColor = this.defaultBlockColor;
                         }
                     }
                 }
+            }
             ,
-                moveItemSideways: function (xDir) {
+            blockCurrentItemInBoard:function(){
+                var currShape = this.currentFallingItem.getShape();//cant do this here if we shall repeat this function....
+                var x = this.currentFallingItem.offsetX;
+                var y = this.currentFallingItem.offsetY;
+                for (var i = 0; i < currShape.length; i += 2) {
+                    boardMap[currShape[i] + x][currShape[i + 1] + y].blocked = true;
+                }
+            },
+            moveItemSideways: function (xDir) {
+                if (!this.isCollidingSideways(xDir)) {
                     this.currentFallingItem.offsetX += xDir;
                 }
+            }
+
+            ,
+            isColliding: function () {
+                //check reach bottom
+                //on each stickfig item in boardMap that matches the currentFalling item in its NEXT position, that is y+1, 
+                //side collision tests must be carried out on events moveItemLeft/right. 
+                var currShape = this.currentFallingItem.getShape(); //cant do this here if we shall repeat this function....
+                var x = this.currentFallingItem.offsetX;
+                var y = this.currentFallingItem.offsetY;
+
+                for (var i = 0; i < currShape.length; i += 2) {
+                    var boardHeight = boardMap[0].length;
+                    if (currShape[i + 1] + y == boardHeight - 1) {
+                        //shouldve reached bottom
+                        return true;
+                    }
+                    if(boardMap[currShape[i] + x][currShape[i + 1] + y +1].blocked){
+                        return true;
+                    }
+                }
+
+                // check collision with blocked item
+
+                return false;
+
+            },
+            isCollidingSideways: function (dirX) {
+                //side collision tests must be carried out on events moveItemLeft/right. 
+                var currShape = this.currentFallingItem.getShape();//cant do this here if we shall repeat this function....
+                var x = this.currentFallingItem.offsetX;
+                var y = this.currentFallingItem.offsetY;
+                var boardWidth = boardMap.length;
+                for (var i = 0; i < currShape.length; i += 2) {
+                    //check board edges
+                    if (dirX > 0) {
+                        if ((currShape[i] + x + dirX) == boardWidth) {
+                            return true;
+                        }
+                    }
+                    else {
+                        if ((currShape[i] + x + dirX) == -1) {
+                            return true;
+                        }
+                    }
+
+                    if (boardMap[currShape[i] + x + dirX][currShape[i + 1] + y].blocked) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            checkScore:function(){
+                //check one row in boardMap at a time, and check if all items are blocked,
+                //then remove those and maybe move all above one step down
+                //just wanna do reduce here....did nt work though l o l 
+                for (var i = 0; i < boardMap[0].length; i++) {
+                    var allblockedonrow = true;
+                    for (var a = boardMap.length-1; a >= 0; a--) {
+                        if (boardMap[a][i].blocked===undefined || boardMap[a][i].blocked == false) { //CHECK THIS LAST ONE
+                            allblockedonrow = false;
+                        }
+                    }
+                    if (allblockedonrow) { //NON WORKING
+                        for (var a = boardMap.length - 1; a >= 0; a--) {
+                            boardMap[a][i].fillColor = this.defaultBlockColor;
+                            boardMap[a][i].blocked = false;
+                            for (var s = i-1; s >= 0; s--) {
+                                if (s > 0 && boardMap[a][s].blocked) { //TODO mUSt chek ALL lines above, EVERYTHING SHALL MOVE DOWN
+                                    //CHECK ABOVE COLS HERE AND MOVE DOWN (chg fillcolor)
+                                    var blItem = boardMap[a][s];
+                                    var foundItemColor = blItem.fillColor;
+                                    blItem.fillColor = this.defaultBlockColor; //delite the one above
+                                    blItem.blocked = false;
+                                    boardMap[a][s+1].fillColor = foundItemColor; //hilite the one were on
+                                    boardMap[a][s+1].blocked = true;
+
+                                }
+
+                            }
+
+                        }
+
+                        //psuedo: go through the row with the hit. For each col check if there is an item attached to the correspondig col above - then move
+                        //that item to where we are. Otherwise just delete it.
+                    }
+                }
+                if (this.currentFallingItem.offsetY < 1) {
+                    alert("game over") //?
+
+                }
+                  
+
+                
+            },
+            initFallingItem: function () {
+                this.state = 1;
+                this.currentFallingItem.variShape = [longy, zed, zedReverse, cube, theL, theLReverse].randItem();
+                this.currentFallingItem.offsetX = 5;
+                this.currentFallingItem.offsetY = 0;
+                this.currentFallingItem.rotation = 0;
+                this.currentFallingItem.color = ['#55ffcc', '#fe44d6', '#fefe55', '#cc56ab', '#ba26eb'].randItem();
+                this.seedFallingItem();
+
+            }
+
 
         }
 
@@ -156,53 +292,116 @@ define(['jquery', 'lib/zz', 'lib/zzUtil', 'lib/zzInteraction','lib/zzDebug'], fu
 
         buttons.forEach(function (b) {
             zzInteraction.bind(b, "mouseover", function (ev, zzItem) {
-                zzItem.fillColor = "#00FFFF";
-                
+                zzItem.fillColor = "#0000FF";
             });
             zzInteraction.bind(b, "mouseout", function (ev, zzItem) {
-                zzItem.fillColor = "#eeeeee";
+                zzItem.fillColor = "#EEEEEE";
             });
         });
 
         zzInteraction.bind(buttons[0], "mousedown", function (ev, zzItem) {
+            //right
             gameControl.moveItemSideways(1);
         });
+        
+
 
         zzInteraction.bind(buttons[1], "mousedown", function (ev, zzItem) {
+            //left
             gameControl.moveItemSideways(-1);
         });
 
         zzInteraction.bind(buttons[2], "mousedown", function (ev, zzItem) {
             //rotate btn
-
-            gameControl.currentFallingItem.rotate();
+          gameControl.currentFallingItem.rotate();
         });
 
         var tmpDate = new Date().getTime();
 
-        gameControl.currentFallingItem.variShape = longy;
-        gameControl.currentFallingItem.offsetX = 5;
-        gameControl.seedFallingItem();
+
+        //draw some cool alphanums....
+        
+
+        function renderStartScreen() {
+            var yposLetter = 59;
+            letterbox.renderLetter(letterbox.alphabet.t, 10, 2, yposLetter);
+            letterbox.renderLetter(letterbox.alphabet.e, 10, 9, yposLetter);
+            letterbox.renderLetter(letterbox.alphabet.t, 10, 14, yposLetter);
+            letterbox.renderLetter(letterbox.alphabet.r, 10, 21, yposLetter);
+            letterbox.renderLetter(letterbox.alphabet.i, 10, 25, yposLetter);
+            letterbox.renderLetter(letterbox.alphabet.s, 10, 30, yposLetter);
+            gameControl.startScreenItems = [];
+            var startButton = new zz.stickFigure(150, 120, 100, 100, "#FEFEFE", "#FFFFFF", zzUtil.blockify(miniCube, 90), 0, { x: 0, y: 0 })
+            zz.world.items.push(
+                startButton
+                );
+            gameControl.startScreenItems.push(startButton);
+            var tControl = new zz.textBlock(175, 160, 200, 30, "START", 44);
+
+
+            zz.world.items.push(tControl);
+            gameControl.startScreenItems.push(tControl);
+            zzInteraction.bind(startButton, "mousedown", function (ev, zzItem) {
+                hideStartScreen();
+                gameControl.initFallingItem();
+            });
+
+
+        }
+        renderStartScreen();
+
+        function hideStartScreen() {
+            gameControl.startScreenItems.forEach(function (a) {
+                a.x = 1000;
+            })
+        }
+
+
+        zz.world.items.push(new zz.textBlock(10, 24, 200, 30, "Tetris by frankfurillo", 44));
 
         zz.run(function (ctx) { //render callback som parameter. Anropas från animate. 
             var elapsed = new Date().getTime() - tmpDate;
             zz.world.items.forEach(function (stickFig) {
-                //  if(stickFig)
-                //if(new Date()
                 stickFig.render(stickFig.onRenderEnd);
-                lastTime = Date.now();
             });
-            gameControl.seedFallingItem();
-            if ((elapsed / 1000) > 0.3) {
-                tmpDate = new Date().getTime();
-                handleFallingItem();
+
+            if (gameControl.state > 0) {
+                gameControl.seedFallingItem();
+                if ((elapsed / 1000) > 0.3) {
+                    tmpDate = new Date().getTime();
+                    handleFallingItem();
+                }
             }
         });
         
 
         function handleFallingItem() {
-            gameControl.currentFallingItem.offsetY++;
+            //check collision before moving
+            if (!gameControl.isColliding()) {
+                gameControl.currentFallingItem.offsetY++;
+            }
+            else {
+                gameControl.blockCurrentItemInBoard();
+                gameControl.checkScore();
+
+                gameControl.initFallingItem();
+            }
         }
+
+        $(document).bind("keydown",function(event){
+            if (event.keyCode === 37) {
+                gameControl.moveItemSideways(-1)
+            }
+            if (event.keyCode === 39) {
+                gameControl.moveItemSideways(1)
+            }
+            if (event.keyCode === 40) {
+                handleFallingItem();
+            }
+            if (event.keyCode === 38) {
+                gameControl.currentFallingItem.rotate();
+            }
+        });
 
     });
 });
